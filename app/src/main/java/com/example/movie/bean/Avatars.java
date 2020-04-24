4d7bfafce4a1b328415de95f33c8e1a01e4a9b8e
qiaoqiao97 <57580261+qiaoqiao97@users.noreply.github.com>
2020-04-24 11:54:25 +0800

    Create Avatars.java

---
+++ app/src/main/java/com/example/movie/bean/Avatars.java
@@ -0,0 +1,38 @@
+/**
+  * Copyright 2020 bejson.com 
+  */
+package com.example.movie.bean;
+
+/**
+ * Auto-generated: 2020-04-18 14:24:3
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Avatars {
+
+    private String small;
+    private String large;
+    private String medium;
+    public void setSmall(String small) {
+         this.small = small;
+     }
+     public String getSmall() {
+         return small;
+     }
+
+    public void setLarge(String large) {
+         this.large = large;
+     }
+     public String getLarge() {
+         return large;
+     }
+
+    public void setMedium(String medium) {
+         this.medium = medium;
+     }
+     public String getMedium() {
+         return medium;
+     }
+
+}
