fbaa41f11a80d88a6b6386d6963d546a23da759e
qiaoqiao97 <57580261+qiaoqiao97@users.noreply.github.com>
2020-04-24 11:58:15 +0800

    Create FourFragment.java

---
+++ app/src/main/java/com/example/movie/fragment/FourFragment.java
@@ -0,0 +1,27 @@
+package com.example.movie.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.movie.R;
+
+public class FourFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.four_fragment_layout, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
