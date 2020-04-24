cf3ea3d690620475b2ca2e4e340ef71ff7406e86
qiaoqiao97 <57580261+qiaoqiao97@users.noreply.github.com>
2020-04-24 11:57:22 +0800

    Create Rating.java

---
+++ app/src/main/java/com/example/movie/bean/Rating.java
@@ -0,0 +1,47 @@
+/**
+  * Copyright 2020 bejson.com 
+  */
+package com.example.movie.bean;
+
+/**
+ * Auto-generated: 2020-04-18 14:24:3
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Rating {
+
+    private int max;
+    private double average;
+    private String stars;
+    private int min;
+    public void setMax(int max) {
+         this.max = max;
+     }
+     public int getMax() {
+         return max;
+     }
+
+    public void setAverage(double average) {
+         this.average = average;
+     }
+     public double getAverage() {
+         return average;
+     }
+
+
+    public void setStars(String stars) {
+         this.stars = stars;
+     }
+     public String getStars() {
+         return stars;
+     }
+
+    public void setMin(int min) {
+         this.min = min;
+     }
+     public int getMin() {
+         return min;
+     }
+
+}
